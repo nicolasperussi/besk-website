aa33ea77e726f94af79160f546d15f2d631cf58c
Nicolas Perussi <nicolasperussi12@hotmail.com>
2025-01-28 15:57:58 -0300

    feat: change spacing on article cards

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { IArticle } from "@/types/article";
 import Image from "next/image";
-import React, { useState } from "react";
-import Modal from "./Modal";
+import React from "react";
 
 interface IArticleCard {
   openArticle: (article: IArticle) => void;
@@ -23,8 +22,14 @@ function ArticleCard({ article, openArticle }: IArticleCard) {
         />
       </div>
       <h1 className="font-medium text-3xl ml-2 mb-2">{title}</h1>
-      <div className="border-y-2 border-black py-5 h-96">
-        <p className="text-lg ml-2">{content[0].text[0]}</p>
+      <div className="border-y-2 border-black py-5 h-72">
+        <ul className="text-lg ml-8 list-disc">
+          {article.content.map((item) => (
+            <li>
+              <strong>{item.title}:</strong> {item.subtitle}
+            </li>
+          ))}
+        </ul>
       </div>
       <div
         className="pt-5 pl-2 cursor-pointer"
