9c578ed161a5eba1e5370eed840c98ce9a75a0c6
Nicolas Perussi <nicolasperussi12@hotmail.com>
2025-01-29 17:51:13 -0300

    feat: add responsivity for header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,74 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Logo from "../../public/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { FaBars, FaXmark } from "react-icons/fa6";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openMenu() {
+    setIsOpen(true);
+  }
+
+  function closeMenu() {
+    setIsOpen(false);
+  }
+
   return (
     <header className="px-5">
-      <div className="py-5 pl-1 pr-4 border-b-2 border-black flex justify-between align-center">
+      <div className="py-5 pl-1 pr-4 border-b-2 border-black flex justify-between items-center">
         <Link href="/" className="w-52">
           <Image priority src={Logo} alt="Bergstein e Skorkowski Advogados" />
         </Link>
-        <div className="hidden xl:flex pr-4 gap-20 font-medium items-center">
-          <Link className="hover:underline" href="/#quem-somos">
+        <div
+          className={`w-screen h-screen absolute ${
+            isOpen ? "left-0" : "-left-full"
+          } top-0 z-50 bg-primary flex flex-col justify-center pr-4 gap-20 font-medium items-center transition-all duration-300 xl:flex xl:w-auto xl:h-auto xl:relative xl:flex-row xl:left-0`}
+        >
+          <FaXmark
+            onClick={closeMenu}
+            className="xl:hidden cursor-pointer absolute right-8 top-12"
+            size={32}
+          />
+          <Link
+            onClick={closeMenu}
+            className="hover:underline"
+            href="/#quem-somos"
+          >
             <h5>Quem Somos</h5>
           </Link>
-          <Link className="hover:underline" href="/#areas-de-atuacao">
+          <Link
+            onClick={closeMenu}
+            className="hover:underline"
+            href="/#areas-de-atuacao"
+          >
             <h5>Áreas de Atuação</h5>
           </Link>
-          <Link className="hover:underline" href="/#advogados">
+          <Link
+            onClick={closeMenu}
+            className="hover:underline"
+            href="/#advogados"
+          >
             <h5>Advogados</h5>
           </Link>
-          <Link className="hover:underline" href="/#conteudo">
+          <Link
+            onClick={closeMenu}
+            className="hover:underline"
+            href="/#conteudo"
+          >
             <h5>Conteúdo</h5>
           </Link>
-          <Link className="hover:underline" href="#contato">
+          <Link onClick={closeMenu} className="hover:underline" href="#contato">
             <h5>Contato</h5>
           </Link>
         </div>
+        <FaBars
+          onClick={openMenu}
+          className="xl:hidden cursor-pointer"
+          size={32}
+        />
       </div>
     </header>
   );
